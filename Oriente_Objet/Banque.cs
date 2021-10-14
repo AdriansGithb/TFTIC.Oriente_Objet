53dfd4211af63be18b0a1da6d331f69f72490089
a.vanhoeke <a.vanhoeke@FORMAVDI1613>
2021-10-14 14:45:32 +0200

    Ajoutez des fichiers projet.

---
+++ Oriente_Objet/Banque.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oriente_Objet
+{
+    class Banque
+    {
+        public string Nom { get; set; }
+        private Dictionary<string, Courant> _Comptes;
+        public Dictionary<string, Courant> Comptes
+        {
+            get { return _Comptes = _Comptes ?? new Dictionary<string, Courant>(); }
+        }
+        public Courant this[string numero]
+        {
+            get
+            {
+                Courant cpt;
+                Comptes.TryGetValue(numero, out cpt);
+                return cpt;
+            }
+            set
+            {
+                if (value != null && value.Numero == numero)
+                    Comptes[numero] = value;
+            }
+        }
+        public void Ajouter(Courant compte)
+        {
+            if (compte != null)
+                Comptes.Add(compte.Numero, compte);
+        }
+        public void Supprimer(string numero)
+        {
+            if (numero != null)
+                Comptes.Remove(numero);
+        }
+        public double AvoirDesComptes(Personne titulaire)
+        {
+            if (titulaire == null)
+                return 0;
+            double result = 0;
+            foreach(Courant cpt in Comptes.Values)
+            {
+                if (cpt.Titulaire == titulaire)
+                    result = cpt + result;
+            }
+            return result;
+        }
+    }
+}
---
+++ Oriente_Objet/Courant.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oriente_Objet
+{
+    class Courant
+    {
+        public string Numero { get; set; }
+        public double Solde { get; private set; }
+        public Personne Titulaire { get; set; }
+        public double _ligneDeCredit;
+        public double LigneDeCredit
+        {
+            get { return _ligneDeCredit; }
+            set
+            {
+                if (value >= 0)
+                    _ligneDeCredit = value;
+                else _ligneDeCredit = 0;
+            }
+        }
+
+        public void Retrait(double montant)
+        {
+            if (montant < 0)
+                return;
+            else
+            {
+                double nwSolde = this.Solde - montant;
+                if (nwSolde >= -LigneDeCredit)
+                    this.Solde -= montant;
+            }
+        }
+        public void Depot(double montant)
+        {
+            this.Solde += montant;
+        }
+        public static double operator +(Courant cpt, double montant)
+        {
+            double result = 0 ;
+            if (montant >= 0)
+                result += cpt.Solde;
+            if (cpt.Solde >= 0)
+                result += montant;
+            return result;
+        }
+    }
+}
